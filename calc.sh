07c07c578590424fa53fd4cd7f7b78dbd416a76b
Ea Francisco <emberkasai04@gmail.com>
2021-11-05 11:35:50 -0400

    Add error message

--- calc.sh
+++ calc.sh
@@ -3,17 +3,21 @@ echo "What do you want to do?
         Add 
         Subtract"
 read OPERATE
-echo "Enter your numbers"
-read NUM_ONE
-read NUM_TWO
 
 case $OPERATE in
     "Add"|"add")
+    echo "Enter your numbers"
+    read NUM_ONE
+    read NUM_TWO
     echo "This is your sum:"
     expr $NUM_ONE + $NUM_TWO
     ;;
     "Subtract"|"subtract")
+    read NUM_ONE
+    read NUM_TWO
     echo "This is your difference:"
     expr $NUM_ONE - $NUM_TWO
     ;;
+    *)
+    echo "Sorry, that's not part of my functions."
 esac
